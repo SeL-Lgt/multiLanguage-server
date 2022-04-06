cb77e39cef6fee8a54939c173abe99a77a37ff55
liguotong <122790345@qq.com>
2022-04-06 11:14:16 +0800

    chore: 添加环境变量配置

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import express, { Express } from 'express';
+import path from 'path';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: path.resolve('envs', `.env.${process.env.NODE_ENV}`),
+});
+const { PORT } = process.env;
 
 const app: Express = express();
 
-app.listen(6000, () => {
-  console.log('Running on http://localhost:6000');
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
 });
