6eb14a6bfc57441ff39efe64a63874fdf4abfd86
liguotong <122790345@qq.com>
2022-04-19 14:37:04 +0800

    feat: 子模块管理接口注释

--- src/router/subModules.ts
+++ src/router/subModules.ts
@@ -3,10 +3,19 @@ import SubModulesServices from '@services/SubModulesServices';
 
 const router = express.Router();
 
+/**
+ * 新增子模块
+ */
 router.post('/addSubModules', SubModulesServices.addSubModules);
 
+/**
+ * 查询子模块列表
+ */
 router.get('/querySubModulesList', SubModulesServices.querySubModulesList);
 
+/**
+ * 删除子模块
+ */
 router.delete('/deleteSubModules', SubModulesServices.deleteSubModules);
 
 const subModulesRouter = router;
