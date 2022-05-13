79140a3b747002a1080901ce97f592ee949e6b5c
liguotong <122790345@qq.com>
2022-05-13 22:07:18 +0800

    feat: 请求实体新增上线10MB

--- src/app.ts
+++ src/app.ts
@@ -28,8 +28,8 @@ const app: Express = express();
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // 挂载路由组件
 app.use('/api', router);
