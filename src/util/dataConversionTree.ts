f77acca4d4896d8c8103e3fc272f51c475b602ea
liguotong <122790345@qq.com>
2022-05-17 16:50:01 +0800

    fix: 修复导出文案json结构不合理的问题

--- src/util/dataConversionTree.ts
+++ src/util/dataConversionTree.ts
@@ -7,6 +7,8 @@ export default function dataToTree(data: object) {
   }
 
   const hashMap: obj = {};
+  // 用于存储 不合理JSON结构，无法转换tree层级 的key值
+  const errorMap: obj = {};
   Object.entries(data).forEach(([key, value]) => {
     let tempMap = hashMap;
     const keyArr = key.split('.');
@@ -15,15 +17,24 @@ export default function dataToTree(data: object) {
       if (!tempMap[curKey]) {
         tempMap[curKey] = {};
       } else if (typeof tempMap[curKey] === 'string') {
-        tempMap[curKey] = {
-          [tempMap[curKey] as string]: tempMap[curKey],
-        };
+        // 判断该层级的值是否为字符串
+        // 将该层级变为空，并将原数据存进错误文案中
+        const lastIndex = key.lastIndexOf('.');
+        const preKey = key.slice(0, lastIndex);
+        errorMap[preKey] = tempMap[curKey];
+        tempMap[curKey] = {};
+        // const lastIndex = key.lastIndexOf('.');
+        // throw new Error(
+        //   `${key.slice(
+        //     0,
+        //     lastIndex,
+        //   )}同时为对象和字符串，为不合理JSON结构，无法转换tree层级，请修改相关key值`,
+        // );
       }
       tempMap = tempMap[curKey] as obj;
     }
-
     tempMap[keyArr[0]] = value;
   });
 
-  return hashMap;
+  return { ...hashMap, ...errorMap };
 }
