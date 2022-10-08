e505db4ad73c9b1fa6150dacd0dbc27f4257bbc6
SeL-Lgt <122790345@qq.com>
2022-10-08 17:20:53 +0800

    chore: 修改数据库环境配置

--- src/util/app-data-source.ts
+++ src/util/app-data-source.ts
@@ -1,33 +1,30 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import dotenv from 'dotenv';
+import path from 'path';
 
-const devDB: DataSourceOptions = {
-  name: 'dev',
+dotenv.config({
+  path: path.resolve('envs', `.env.${process.env.NODE_ENV}`),
+});
+
+const { NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } =
+  process.env;
+
+const dbConfig: DataSourceOptions = {
+  name: NODE_ENV,
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '981002',
-  database: 'language_i18n',
+  host: DB_HOST,
+  port: DB_PORT as unknown as number,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
   entities: [`${__dirname}/../entity/**/*{.js,.ts}`],
   namingStrategy: new SnakeNamingStrategy(),
-  synchronize: true,
+  connectTimeout: 60000,
+  acquireTimeout: 60000,
+  // synchronize: true,
 };
 
-const prodDB: DataSourceOptions = {
-  name: 'prod',
-  type: 'mysql',
-  host: '106.55.241.206',
-  port: 3306,
-  username: 'test',
-  password: '981002',
-  database: 'language_i18n',
-  entities: [`${__dirname}/../entity/**/*{.js,.ts}`],
-  namingStrategy: new SnakeNamingStrategy(),
-};
-
-const dataSource = new DataSource(
-  process.env.NODE_ENV === 'prod' ? prodDB : devDB,
-);
+const dataSource = new DataSource(dbConfig);
 
 export default dataSource;
